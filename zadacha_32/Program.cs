51af2fd7a3e121e8f5b775c2f917e13b25a6732e
Larina <Losiknor@gmail.com>
2022-02-17 00:41:32 +0700

    Решение задачи 32

--- zadacha_32/Program.cs
+++ zadacha_32/Program.cs
@@ -1,3 +1,10 @@
 ﻿// Задача 32: Задайте массив из восьми элементов, заполненных нулями 
 // и единицами. Выведите их на экран
-Console.WriteLine("Hello, World!");
+int[] array = new int[8];
+Random rnd1 = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd1.Next(0, 2);
+    Console.Write(array[i] + " ");
+}

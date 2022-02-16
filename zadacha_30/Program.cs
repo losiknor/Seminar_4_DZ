22dec9aab66482e046bdae310c14e103fdb6714c
Larina <Losiknor@gmail.com>
2022-02-17 00:24:33 +0700

    Решение задачи 30

--- zadacha_30/Program.cs
+++ zadacha_30/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Введите число: ");
+//int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Кубы чисел от 1 до 20, заканчивающиеся на чётную цифру: ");
+for (int i = 1; i <= 20; i++)
+{
+    double res = Math.Pow(i, 3);
+    if ((res % 10) % 2 == 0)
+        Console.WriteLine(res);
+
+}
+

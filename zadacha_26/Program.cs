1834e82588c6efc22940929ec420714eda0313d7
Larina <Losiknor@gmail.com>
2022-02-16 17:23:05 +0700

    Решение задачи 26

--- zadacha_26/Program.cs
+++ zadacha_26/Program.cs
@@ -1,3 +1,8 @@
 ﻿// Задача 26: Возведите число А в натуральную степень B, используя цикл.
-int num = 3;
-Console.WriteLine(Math.Pow(3, 3));
+int num = new Random().Next(2, 10);
+int deg = new Random().Next(2, 10);
+Console.WriteLine("Результат возведения " + num + " в степень " + deg +" : ");
+int exp = 1;
+for (int i = 1; i <= deg; i++)
+exp = exp * num;
+Console.Write(exp);

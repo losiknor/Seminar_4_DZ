6f9fa4c257fd18e7e5dca8d4fc2f9c813e2f817a
Larina <Losiknor@gmail.com>
2022-02-16 23:30:12 +0700

    Решение задачи 28

--- zadacha_28/Program.cs
+++ zadacha_28/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Задача 28: Подсчитайте сумму цифр в числе
-Console.WriteLine("Hello, World!");
+int number = new Random().Next(1, 1000000);
+int number1 = number;
+int sum = 0;
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+Console.WriteLine("Сумма цифр в числе " + number1 + " : " + sum);
